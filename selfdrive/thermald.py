856f7df68646bc0ccc335d6d6d2fe2c28f04d5b3
multikyd <55026351+multikyd@users.noreply.github.com>
2020-04-07 00:59:20 +0900

    Revert "Update thermald.py"
    This reverts commit 4c39690695eeb0c4b78d8451cd9ef88c9df4baba.

--- selfdrive/thermald.py
+++ selfdrive/thermald.py
@@ -18,6 +18,7 @@ from selfdrive.swaglog import cloudlog
 import cereal.messaging as messaging
 from selfdrive.loggerd.config import get_available_percent
 from selfdrive.pandad import get_expected_signature
+import pygame
 from selfdrive.kegman_conf import kegman_conf
 kegman = kegman_conf()
 
@@ -379,6 +380,12 @@ def thermald_thread():
       if off_ts is None:
         off_ts = sec_since_boot()
         os.system('echo powersave > /sys/class/devfreq/soc:qcom,cpubw/governor')
+
+      if msg.thermal.batteryStatus == "Discharging" and \
+         started_seen and (sec_since_boot() - off_ts) > 5:
+        pygame.mixer.init()
+        s = pygame.mixer.Sound("takeaway.wav")
+        s.play()
 
       # shutdown if the battery gets lower than 3%, it's discharging, we aren't running for
       # more than a minute but we were running
