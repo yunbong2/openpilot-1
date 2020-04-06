e7a798725d4dd491f9e2f147a082ffc16aeb1889
multikyd <55026351+multikyd@users.noreply.github.com>
2020-04-07 00:13:45 +0900

    Update thermald.py

--- selfdrive/thermald.py
+++ selfdrive/thermald.py
@@ -18,8 +18,7 @@ from selfdrive.swaglog import cloudlog
 import cereal.messaging as messaging
 from selfdrive.loggerd.config import get_available_percent
 from selfdrive.pandad import get_expected_signature
-from pydub import AudioSegment
-from pydub.playback import play
+import pygame
 from selfdrive.kegman_conf import kegman_conf
 kegman = kegman_conf()
 
@@ -384,8 +383,9 @@ def thermald_thread():
 
       if msg.thermal.batteryStatus == "Discharging" and \
          started_seen and (sec_since_boot() - off_ts) > 5:
-        song = AudioSegment.from_wav("takeaway.wav")
-        play(song) 
+        pygame.mixer.init()
+        s = pygame.mixer.Sound("takeaway.wav")
+        s.play()
 
       # shutdown if the battery gets lower than 3%, it's discharging, we aren't running for
       # more than a minute but we were running
