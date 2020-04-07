b356b1de41f181133af55ee3a0805964de3d039f
multikyd <55026351+multikyd@users.noreply.github.com>
2020-04-07 09:36:56 +0900

    update

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -48,8 +48,8 @@ class CarInterface(CarInterfaceBase):
     ret.safetyModel = car.CarParams.SafetyModel.hyundai
     ret.enableCruise = True  # stock acc
 
-    ret.steerActuatorDelay = 0.1  # Default delay
-    ret.steerRateCost = 0.5
+    ret.steerActuatorDelay = 0.15  # Default delay
+    ret.steerRateCost = 0.45
     ret.steerLimitTimer = 0.8
     tire_stiffness_factor = 0.75
 
