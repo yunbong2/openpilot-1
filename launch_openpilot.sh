f9dd5bb1385e93c6a698ab1a2a04cb8eda3eb796
multikyd <55026351+multikyd@users.noreply.github.com>
2020-04-09 22:36:26 +0900

    update

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/bash
 
+if [ ! -f /data/ota_updates ]; then
+    /usr/bin/touch /data/ota_updates
+fi
 /usr/bin/sh /data/openpilot/kyd/fonts/installer.sh &
 export PASSIVE="0"
 exec ./launch_chffrplus.sh
