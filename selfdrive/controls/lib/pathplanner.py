46447e68468335dd3a2911a787d2c3c4cf7ca8cd
multikyd <multikyd@gmail.com>
2020-05-04 11:52:46 +0900

    Update pathplanner.py

--- selfdrive/controls/lib/pathplanner.py
+++ selfdrive/controls/lib/pathplanner.py
@@ -173,11 +173,11 @@ class PathPlanner():
 
       if self.lane_change_direction == LaneChangeDirection.left:
         torque_applied = sm['carState'].steeringTorque > 0 and sm['carState'].steeringPressed
-        if CP.autoLcaEnabled and 1.6 > self.pre_auto_LCA_timer > 1.1 and not lca_left:
+        if CP.autoLcaEnabled and 1.5 > self.pre_auto_LCA_timer > 1.0 and not lca_left:
           torque_applied = True # Enable auto LCA only once after 1 sec 
       else:
         torque_applied = sm['carState'].steeringTorque < 0 and sm['carState'].steeringPressed
-        if CP.autoLcaEnabled and 1.6 > self.pre_auto_LCA_timer > 1.1 and not lca_right:
+        if CP.autoLcaEnabled and 1.5 > self.pre_auto_LCA_timer > 1.0 and not lca_right:
           torque_applied = True # Enable auto LCA only once after 1 sec 
 
       lane_change_prob = self.LP.l_lane_change_prob + self.LP.r_lane_change_prob
@@ -302,7 +302,8 @@ class PathPlanner():
       self.solution_invalid_cnt = 0
     plan_solution_valid = self.solution_invalid_cnt < 2
 
-    plan_send = messaging.new_message('pathPlan')
+    plan_send = messaging.new_message()
+    plan_send.init('pathPlan')
     plan_send.valid = sm.all_alive_and_valid(service_list=['carState', 'controlsState', 'liveParameters', 'model'])
     plan_send.pathPlan.laneWidth = float(self.LP.lane_width)
     plan_send.pathPlan.dPoly = [float(x) for x in self.LP.d_poly]
@@ -326,8 +327,8 @@ class PathPlanner():
 
     pm.send('pathPlan', plan_send)
 
-    if LOG_MPC:
-      dat = messaging.new_message('liveMpc')
+      dat = messaging.new_message()
+      dat.init('liveMpc')
       dat.liveMpc.x = list(self.mpc_solution[0].x)
       dat.liveMpc.y = list(self.mpc_solution[0].y)
       dat.liveMpc.psi = list(self.mpc_solution[0].psi)
