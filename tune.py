19847b7afd6a1310bdf2368a4872a5b2ae7fd24a
multikyd <multikyd@gmail.com>
2020-05-05 20:50:03 +0900

    Update tune.py

--- tune.py
+++ tune.py
@@ -137,7 +137,12 @@ while True:
       j = len(param) - 1
 
   elif (char == "p"):
-    os.system('LD_LIBRARY_PATH="" svc power reboot')
+    print ("system will reboot. continue?(y/n)")
+    char2 = getch()
+    if (char2 == "y"):
+      os.system('LD_LIBRARY_PATH="" svc power reboot')
+    else:
+      process.kill()
 
   elif (char == "z"):
     process.kill()
