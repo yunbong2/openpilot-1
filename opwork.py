442f496ff2ccc09d932b151cc1575a5015a0835b
multikyd <multikyd@gmail.com>
2020-05-05 23:19:59 +0900

    Update opwork.py

--- opwork.py
+++ opwork.py
@@ -34,7 +34,8 @@ char  = getch()
 
 if (char == "1"):
     ct = time.strftime('%Y%m%d%H%M%S', time.localtime(time.time()))
-    os.system('cp -rf /data/openpilot /data/openpilot_%ct')
+    print ("Copying openpilot to openpilot_"ct)
+    os.system("cp -rf /data/openpilot /data/openpilot_" + ct)
 
 elif (char == "z"):
     ct = time.strftime('%Y%m%d%H%M%S', time.localtime(time.time()))
