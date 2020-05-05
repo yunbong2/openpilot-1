afc0539e8788a115520fd7fb3a6bfaad9e706d0b
multikyd <multikyd@gmail.com>
2020-05-05 22:55:54 +0900

    Update opwork.py

--- opwork.py
+++ opwork.py
@@ -20,26 +20,26 @@ def getch():
 
 button_delay = 0.2
 
-  print ("Please select job what you want(number).")
-  print ("")
-  print ("1. OPBACKUP   - copy your openpilot directory to openpilot_(datetimestamp) and still remain openpilot dir")
-  print ("2. OPINSTALL  - install the openpilot directory. if exist OP direcoty, rename openpilot to openpilot_(datetimestamp")
-  print ("3. OPUPDATE   - run /"git pull/" command to update OP latest")
-  print ("4. OPRESTORE  - restore openpilot with current OP directory referring timestamp")
-  print ("5. CHBRANCH   - branch change")
-  print ("6. SEEBRANCH  - confirm current branch")
-  print ("7. SEEFORK    - confirm current fork")
-  print ("z. EXIT)
+print ("Please select job what you want(number).")
+print ("")
+print ("1. OPBACKUP   - copy your openpilot directory to openpilot_(datetimestamp) and still remain openpilot dir")
+print ("2. OPINSTALL  - install the openpilot directory. if exist OP direcoty, rename openpilot to openpilot_(datetimestamp")
+print ("3. OPUPDATE   - run /"git pull/" command to update OP latest")
+print ("4. OPRESTORE  - restore openpilot with current OP directory referring timestamp")
+print ("5. CHBRANCH   - branch change")
+print ("6. SEEBRANCH  - confirm current branch")
+print ("7. SEEFORK    - confirm current fork")
+print ("z. EXIT)
 
 
-  char  = getch()
+char  = getch()
 
-  if (char == "1"):
+if (char == "1"):
     ct = time.strftime('%Y%m%d%H%M%S', time.localtime(time.time()))
     print (ct)
 
-  elif (char == "z"):
+elif (char == "z"):
     process.kill()
     break
 
-  time.sleep(button_delay)
+time.sleep(button_delay)
