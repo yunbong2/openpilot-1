607ee7a42837aea0757e7110420bc2f0e5f985c0
opbykyd <opbykyd@gmail.com>
2020-05-11 12:47:13 +0900

    update

--- panda/board/safety/safety_gm.h
+++ panda/board/safety/safety_gm.h
@@ -18,7 +18,7 @@ typedef struct {
 } gm_dual_buffer;
 
 const int GM_MAX_STEER = 300;
-const int GM_MAX_RT_DELTA = 64;          // max delta torque allowed for real time checks
+const int GM_MAX_RT_DELTA = 128;          // max delta torque allowed for real time checks
 const uint32_t GM_RT_INTERVAL = 250000;    // 250ms between real time checks
 const int GM_MAX_RATE_UP = 7;
 const int GM_MAX_RATE_DOWN = 17;
@@ -149,12 +149,12 @@ static int gm_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
     //}
 
     // exit controls on regen paddle
-    if (addr == 189) {
-      bool regen = GET_BYTE(to_push, 0) & 0x20;
-      if (regen) {
-        controls_allowed = 0;
-      }
-    }
+    //if (addr == 189) {
+    //  bool regen = GET_BYTE(to_push, 0) & 0x20;
+    //  if (regen) {
+    //    controls_allowed = 0;
+    //  }
+    //}
 
     // Check if ASCM or LKA camera are online
     // on powertrain bus.
