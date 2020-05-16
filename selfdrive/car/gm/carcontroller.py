3e03ea67c127e4f07c483d1242300ad5480b1a63
multikyd <multikyd@gmail.com>
2020-05-16 16:34:37 +0900

    update

--- selfdrive/car/gm/carcontroller.py
+++ selfdrive/car/gm/carcontroller.py
@@ -19,10 +19,10 @@ class CarControllerParams():
       self.STEER_DELTA_DOWN = 5        # 0.3s from peak torque to zero
       self.MIN_STEER_SPEED = -1.       # can steer down to zero
     else:
-      self.STEER_MAX = 300
+      self.STEER_MAX = 270
       self.STEER_STEP = 1.5             # how often we update the steer cmd
-      self.STEER_DELTA_UP = 4           # ~0.75s time to peak torque (255/50hz/0.75s)
-      self.STEER_DELTA_DOWN = 7       # ~0.3s from peak torque to zero
+      self.STEER_DELTA_UP = 3           # ~0.75s time to peak torque (255/50hz/0.75s)
+      self.STEER_DELTA_DOWN = 3       # ~0.3s from peak torque to zero
       self.MIN_STEER_SPEED = -1.
 
     self.STEER_DRIVER_ALLOWANCE = 50   # allowed driver torque before start limiting
