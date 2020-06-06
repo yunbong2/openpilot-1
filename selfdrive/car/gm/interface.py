ed009cd1e31513bd3b085f269832ac1fc47aff9b
multikyd <multikyd@gmail.com>
2020-06-06 23:37:27 +0900

    Update interface.py

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -386,10 +386,10 @@ class CarInterface(CarInterfaceBase):
       #if (ret.gasPressed and not self.gas_pressed_prev) or \
       # (ret.brakePressed): # and (not self.brake_pressed_prev or ret.vEgo > 0.001)):
       #  events.append(create_event('pedalPressed', [ET.NO_ENTRY, ET.USER_DISABLE]))
-      if ret.brakePressed:
-        events.append(create_event('pedalPressed', [ET.NO_ENTRY, ET.USER_DISABLE]))
-      if ret.gasPressed and self.cruise_enabled_prev:
-        events.append(create_event('pcmEnable', [ET.ENABLE]))
+      #if ret.brakePressed:
+      #  events.append(create_event('pedalPressed', [ET.NO_ENTRY, ET.USER_DISABLE]))
+      #if ret.gasPressed and self.cruise_enabled_prev:
+      #  events.append(create_event('pcmEnable', [ET.ENABLE]))
       if ret.cruiseState.standstill:
         events.append(create_event('resumeRequired', [ET.WARNING]))
       #if not self.CS.car_fingerprint in NO_ASCM_CARS:
