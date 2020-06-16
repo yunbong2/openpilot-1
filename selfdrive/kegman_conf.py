1534d3bd0bce5bf1c6b22b92abf02baa6688183e
opbykyd <opbykyd@gmail.com>
2020-06-16 18:17:49 +0900

    Update kegman_conf.py

--- selfdrive/kegman_conf.py
+++ selfdrive/kegman_conf.py
@@ -14,15 +14,18 @@ class kegman_conf():
       write_conf = True
 	
     # only fetch Kp, Ki, Kf sR and sRC from interface.py if it's a PID controlled car
-    if CP.lateralTuning.which() == 'pid':
-      if self.conf['Kp'] == "-1":
-        self.conf['Kp'] = str(round(CP.lateralTuning.pid.kpV[0],3))
+    if CP.lateralTuning.which() == 'indi':
+      if self.conf['outerLG'] == "-1":
+        self.conf['outerLG'] = str(round(CP.lateralTuning.indi.outerLoopGain,2))
         write_conf = True
-      if self.conf['Ki'] == "-1":
-        self.conf['Ki'] = str(round(CP.lateralTuning.pid.kiV[0],3))
+      if self.conf['innerLG'] == "-1":
+        self.conf['innerLG'] = str(round(CP.lateralTuning.indi.innerLoopGain,2))
         write_conf = True
-      if self.conf['Kf'] == "-1":
-        self.conf['Kf'] = str('{:f}'.format(CP.lateralTuning.pid.kf))
+      if self.conf['timeConst'] == "-1":
+        self.conf['timeConst'] = str(round(CP.lateralTuning.indi.timeConstant,2))
+        write_conf = True
+      if self.conf['actEffect'] == "-1":
+        self.conf['actEffect'] = str(round(CP.lateralTuning.indi.actuatorEffectiveness,2))
         write_conf = True
     
     if self.conf['steerRatio'] == "-1":
@@ -55,8 +58,13 @@ class kegman_conf():
 
       if "tuneGernby" not in self.config:
         self.config.update({"tuneGernby":"1"})
-        self.config.update({"Kp":"-1"})
-        self.config.update({"Ki":"-1"})
+        self.element_updated = True
+
+      if "outerLG" not in self.config:
+        self.config.update({"outerLG":"-1"})
+        self.config.update({"innerLG":"-1"})
+        self.config.update({"timeConst":"-1"})
+        self.config.update({"actEffect":"-1"})
         self.element_updated = True
 
       if "liveParams" not in self.config:
@@ -67,13 +75,21 @@ class kegman_conf():
         self.config.update({"steerRatio":"-1"})
         self.config.update({"steerRateCost":"-1"})
         self.element_updated = True
+
+      if "steerMax" not in self.config:
+        self.config.update({"steerMax":"300"})
+        self.element_updated = True
+
+      if "deltaUp" not in self.config:
+        self.config.update({"deltaUp":"3"})
+        self.element_updated = True
+
+      if "deltaDown" not in self.config:
+        self.config.update({"deltaDown":"3"})
+        self.element_updated = True
 	
       if "leadDistance" not in self.config:
         self.config.update({"leadDistance":"5"})
-        self.element_updated = True
-	
-      if "deadzone" not in self.config:
-        self.config.update({"deadzone":"2.0"})
         self.element_updated = True
 	
       if "1barBP0" not in self.config:
@@ -84,7 +100,6 @@ class kegman_conf():
         self.config.update({"3barBP0":"0.0"})
         self.config.update({"3barBP1":"3.0"})
         self.element_updated = True
-
 
       if "1barMax" not in self.config:
         self.config.update({"1barMax":"2.1"})
@@ -100,10 +115,6 @@ class kegman_conf():
 	
       if "slowOnCurves" not in self.config:
         self.config.update({"slowOnCurves":"0"})
-        self.element_updated = True
-	
-      if "Kf" not in self.config:
-        self.config.update({"Kf":"-1"})
         self.element_updated = True
 	
       if "sR_boost" not in self.config:
@@ -136,13 +147,14 @@ class kegman_conf():
 
     else:
       self.config = {"cameraOffset":"0.06", "lastTrMode":"1", "battChargeMin":"60", "battChargeMax":"70", \
-                     "wheelTouchSeconds":"86400", "battPercOff":"100", "carVoltageMinEonShutdown":"11800", \
+                     "wheelTouchSeconds":"30000", "battPercOff":"100", "carVoltageMinEonShutdown":"11800", \
                      "brakeStoppingTarget":"0.25", "tuneGernby":"1", \
-                     "Kp":"-1", "Ki":"-1", "liveParams":"1", "leadDistance":"5", "deadzone":"2.0", \
+                     "outerLG":"-1", "innerLG":"-1", "timeConst":"-1", "actEffect":"-1", \
+                     "liveParams":"1", "leadDistance":"5", "steerMax":"300", "deltaUp":"3", "deltaDown":"3", \
                      "1barBP0":"-0.1", "1barBP1":"2.25", "2barBP0":"-0.1", "2barBP1":"2.5", "3barBP0":"0.0", \
                      "3barBP1":"3.0", "1barMax":"2.1", "2barMax":"2.1", "3barMax":"2.1", \
                      "1barHwy":"0.4", "2barHwy":"0.3", "3barHwy":"0.1", \
-                     "steerRatio":"-1", "steerRateCost":"-1", "slowOnCurves":"0", "Kf":"-1", \
+                     "steerRatio":"-1", "steerRateCost":"-1", "slowOnCurves":"0", \
                      "sR_boost":"0", "sR_BP0":"0", "sR_BP1":"0", "sR_time":"0.2", \
                      "ALCnudgeLess":"1", "ALCminSpeed":"16.666667", "ALCtimer":"1.0", "CruiseDelta":"8", \
                      "CruiseEnableMin":"0"}
