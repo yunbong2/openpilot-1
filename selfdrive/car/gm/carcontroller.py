9f453e2770535ccdf3a89bd9a7dc6c91a70398c8
opbykyd <opbykyd@gmail.com>
2020-06-16 18:20:38 +0900

    Update carcontroller.py

--- selfdrive/car/gm/carcontroller.py
+++ selfdrive/car/gm/carcontroller.py
@@ -6,12 +6,14 @@ from selfdrive.car import apply_std_steer_torque_limits
 from selfdrive.car.gm import gmcan
 from selfdrive.car.gm.values import DBC, AccState, SUPERCRUISE_CARS, NO_ASCM_CARS
 from opendbc.can.packer import CANPacker
+from selfdrive.kegman_conf import kegman_conf
 
 VisualAlert = car.CarControl.HUDControl.VisualAlert
 
 
 class CarControllerParams():
   def __init__(self, car_fingerprint):
+    kegman = kegman_conf()
     if car_fingerprint in SUPERCRUISE_CARS:
       self.STEER_MAX = 150
       self.STEER_STEP = 1              # how often we update the steer cmd
@@ -19,10 +21,10 @@ class CarControllerParams():
       self.STEER_DELTA_DOWN = 5        # 0.3s from peak torque to zero
       self.MIN_STEER_SPEED = -1.       # can steer down to zero
     else:
-      self.STEER_MAX = 300
+      self.STEER_MAX = int(kegman.conf['steerMax'])
       self.STEER_STEP = 1             # how often we update the steer cmd
-      self.STEER_DELTA_UP = 3           # ~0.75s time to peak torque (255/50hz/0.75s)
-      self.STEER_DELTA_DOWN = 3       # ~0.3s from peak torque to zero
+      self.STEER_DELTA_UP = int(kegman.conf['deltaUp'])           # ~0.75s time to peak torque (255/50hz/0.75s)
+      self.STEER_DELTA_DOWN = int(kegman.conf['deltaDown'])       # ~0.3s from peak torque to zero
       self.MIN_STEER_SPEED = -1.
 
     self.STEER_DRIVER_ALLOWANCE = 50   # allowed driver torque before start limiting
