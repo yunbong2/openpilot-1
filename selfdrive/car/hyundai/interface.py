4515b0f9a9fb3ec543cb577acebb7648b30638ae
multikyd <multikyd@gmail.com>
2020-06-17 22:56:30 +0900

    update

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -90,6 +90,18 @@ class CarInterface(CarInterfaceBase):
       ret.steerRatio = 13.0
       ret.mass = 2060. + STD_CARGO_KG
       ret.wheelbase = 3.01
+    elif candidate == CAR.GENESIS_G70:
+      ret.lateralTuning.init('indi')
+      ret.lateralTuning.indi.innerLoopGain = 3.0
+      ret.lateralTuning.indi.outerLoopGain = 2.0
+      ret.lateralTuning.indi.timeConstant = 1.0
+      ret.lateralTuning.indi.actuatorEffectiveness = 1.5
+      ret.steerActuatorDelay = 0.08 # Stinger GT Limited AWD 3.3T stock value (Tunder's 2020) 
+      ret.steerLimitTimer = 0.4 # stock is 0.01 but 0.04 seems to work well
+      ret.steerRateCost = 1.0
+      ret.steerRatio = 12.0
+      ret.mass = 1725. + STD_CARGO_KG
+      ret.wheelbase = 2.835
     elif candidate in [CAR.K5, CAR.SONATA]:
       ret.lateralTuning.init('indi')
       ret.lateralTuning.indi.innerLoopGain = 3.0
