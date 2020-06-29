e42c3fcbf6bdb188cb057f60f68d123ca2901a27
chobyounghak <61299886+chobyounghak@users.noreply.github.com>
2020-06-30 00:31:02 +0900

    dd

--- panda/board/safety/safety_hyundai_073.h
+++ panda/board/safety/safety_hyundai_073.h
@@ -11,7 +11,7 @@ const CanMsg HYUNDAI_TX_MSGS[] = {
   {1265, 0, 4},{1265, 1, 4}, {1265, 2, 4},  // CLU11 Bus 0, 1, 2
   {1157, 0, 4}, // LFAHDA_MFC Bus 0
   {593, 2, 8},  // MDPS12, Bus 2
-  // {1056, 0, 8}, //   SCC11,  Bus 0
+  {1056, 0, 8}, //   SCC11,  Bus 0
   {1057, 0, 8},  //   SCC12,  Bus 0
 
   // {1290, 0, 8}, //   SCC13,  Bus 0
