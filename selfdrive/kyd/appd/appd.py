cbe59b2319f3cf8e2760572cfb6a9e612eb89d65
multikyd <multikyd@gmail.com>
2020-07-02 23:17:08 +0900

    Update appd.py

--- selfdrive/kyd/appd/appd.py
+++ selfdrive/kyd/appd/appd.py
@@ -12,8 +12,8 @@ params = Params()
 mixplorer = "com.mixplorer"
 mixplorer_main = "com.mixplorer.activities.BrowseActivity"
 
-quickedit = "com.rhmsoft.edit"
-quickedit_main = "com.rhmsoft.edit.activity.MainActivity"
+quickedit = "com.rhmsoft.edit_pro"
+quickedit_main = "com.rhmsoft.edit_pro.activity.MainActivity"
 
 offroad = "ai.comma.plus.offroad"
 offroad_main = ".MainActivity"
