d0e2d9c54b8851de9ef42aea259cc0c46ec5ac0d
multikyd <multikyd@gmail.com>
2020-07-11 00:12:45 +0900

    update

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -292,7 +292,7 @@ class CarController():
     lkas_active = enabled and abs(CS.out.steeringAngle) < 90. #and self.lkas_button
 
     # fix for Genesis hard fault at low speed
-    if CS.out.vEgo < 16.666667 and self.car_fingerprint == CAR.GENESIS:
+    #if CS.out.vEgo < 16.666667 and self.car_fingerprint == CAR.GENESIS:
       lkas_active = 0
 
     if not lkas_active:
