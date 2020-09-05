93d200a89a8d4df4494529238eb00eb43881f9b1
yunbong2 <49453243+yunbong2@users.noreply.github.com>
2020-09-06 02:20:27 +0900

    Update driver_monitor.py

--- selfdrive/controls/lib/driver_monitor.py
+++ selfdrive/controls/lib/driver_monitor.py
@@ -8,10 +8,10 @@ from selfdrive.kegman_conf import kegman_conf
 kegman = kegman_conf()
 
 
-_AWARENESS_TIME = min(int(kegman.conf['wheelTouchSeconds']), 600)    # x minutes limit without user touching steering wheels make the car enter a terminal status
+_AWARENESS_TIME = min(int(kegman.conf['wheelTouchSeconds']), 6000)    # x minutes limit without user touching steering wheels make the car enter a terminal status
 _AWARENESS_PRE_TIME_TILL_TERMINAL = 25.  # a first alert is issued 25s before expiration
 _AWARENESS_PROMPT_TIME_TILL_TERMINAL = 15.  # a second alert is issued 15s before start decelerating the car
-_DISTRACTED_TIME = 11.
+_DISTRACTED_TIME = 1100.
 _DISTRACTED_PRE_TIME_TILL_TERMINAL = 8.
 _DISTRACTED_PROMPT_TIME_TILL_TERMINAL = 6.
 
